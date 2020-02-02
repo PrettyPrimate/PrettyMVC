d252bd51833b9b195aaed27958f2382b488a21e7
Bubbs <david@watchfinder.co.uk>
2020-02-02 18:24:20 +0000

    first

---
+++ PrettyMVC/PrettyMVC/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PrettyMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+
+
+
+
+            return View();
+        }
+
+        public JsonResult GetClubDetails()
+        {
+
+            return new JsonResult();
+        }
+
+        //[HttpPost]
+        //public async Task<JsonResult> UpdateTarget(UpdateStaffSalesTarget command)
+        //{
+        //    return this.Json(await this.ExecuteJSONCommand(command), JsonRequestBehavior.AllowGet);
+        //}
+
+        public async Task<ActionResult> GetClubs()
+        {
+            return new EmptyResult();
+        }
+    }
+}
